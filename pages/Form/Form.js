9de81e8ae7f83495d059a5b0f177ce1852ac79c1
KeepFing <bear393048@163.com>
2018-04-04 16:31:47 +0800

    Add files via upload

---
+++ pages/Form/Form.js
@@ -0,0 +1,106 @@
+Page({
+  data: {
+    showTopTips: false,
+
+    radioItems: [
+      { name: 'cell standard', value: '0' },
+      { name: 'cell standard', value: '1', checked: true }
+    ],
+    checkboxItems: [
+      { name: 'standard is dealt for u.', value: '0', checked: true },
+      { name: 'standard is dealicient for u.', value: '1' }
+    ],
+
+    date: "2018-03-29",
+    time: "12:00",
+
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
+
+    countries: ["中国", "美国", "英国", "俄罗斯", "法国", "德国", "意大利", "墨西哥", "加拿大", "刚果", "南非", "巴西", "阿根廷", "西班牙", "埃及", "巴基斯坦", "土耳其", "刚果", "澳大利亚", "印度", "泰国", "比利时", "瑞士"],
+    countryIndex: 0,
+
+    accounts: ["微信", "QQ", "Email", "手机"],
+    accountIndex: 0,
+
+    isAgree: false
+  },
+  showTopTips: function () {
+    var that = this;
+    this.setData({
+      showTopTips: true
+    });
+    setTimeout(function () {
+      that.setData({
+        showTopTips: false
+      });
+    }, 3000);
+  },
+  radioChange: function (e) {
+    console.log('radio发生change事件，携带value值为：', e.detail.value);
+
+    var radioItems = this.data.radioItems;
+    for (var i = 0, len = radioItems.length; i < len; ++i) {
+      radioItems[i].checked = radioItems[i].value == e.detail.value;
+    }
+
+    this.setData({
+      radioItems: radioItems
+    });
+  },
+  checkboxChange: function (e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+    var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+    for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+      checkboxItems[i].checked = false;
+
+      for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (checkboxItems[i].value == values[j]) {
+          checkboxItems[i].checked = true;
+          break;
+        }
+      }
+    }
+
+    this.setData({
+      checkboxItems: checkboxItems
+    });
+  },
+  bindDateChange: function (e) {
+    this.setData({
+      date: e.detail.value
+    })
+  },
+  bindTimeChange: function (e) {
+    this.setData({
+      time: e.detail.value
+    })
+  },
+  bindCountryCodeChange: function (e) {
+    console.log('picker country code 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryCodeIndex: e.detail.value
+    })
+  },
+  bindCountryChange: function (e) {
+    console.log('picker country 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      countryIndex: e.detail.value
+    })
+  },
+  bindAccountChange: function (e) {
+    console.log('picker account 发生选择改变，携带值为', e.detail.value);
+
+    this.setData({
+      accountIndex: e.detail.value
+    })
+  },
+  bindAgreeChange: function (e) {
+    this.setData({
+      isAgree: !!e.detail.value.length
+    });
+  }
+});
---
+++ pages/index/index.js
@@ -0,0 +1,66 @@
+// pages/index/index.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    grid: [{ name: 'Button', url: '../../pages/Button/Button' }, { name: 'Gird', url: '../../pages/Gird/Gird' }, { name: 'Tabs', url: '../../pages/Tabs/Tabs' }, { name: 'Carousel', url:'../../pages/Carousel/Carousel'}, { name: 'List' , url:'../../pages/List/List'}, { name: 'Badge' ,url:'../../pages/Badge/Badge'}, { name: 'Form' ,url:'../../pages/Form/Form'}, { name: '待续。。。' }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
